69f48d6916ad39afef44509329b891156ea0919d
JB243 <55747737+JB243@users.noreply.github.com>
2026-04-01 13:12:50 -0400

    Update COMPOSITION.py

--- src/COMPOSITION.py
+++ src/COMPOSITION.py
@@ -2366,7 +2366,7 @@ def viz_annot_celltype_niche(model_ff, cell_types_vae, cell_types_obs, save=Fals
                 pred_label_map[int(vae_ct)] = default_label
     
         return pred_label_map
-    pred_label_map = make_pred_label_map_(cell_types_obs2, cell_types_obs)
+    pred_label_map = make_pred_label_map_(cell_types_vae, cell_types_obs)
     
     # 3) significant/nonsignificant topic index 
     significant_topics = set(range(0, 32)) # modify it if needed e.g., {3,4,6,14}
